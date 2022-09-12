9b98d05d6e62814958a640f66a80ac850a05f747
Jorge Leonard-Pinero <itoleonard@gmail.com>
2022-09-13 11:25:14 +1200

    Changed name space to RazorPagesPizza.Pages

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPagesPizza.pages
+namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
