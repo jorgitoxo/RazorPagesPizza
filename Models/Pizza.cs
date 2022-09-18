86702ab330fe955825f1d8422628df01999a2fc0
Jorge Leonard-Pinero <itoleonard@gmail.com>
2022-09-18 17:00:41 +1200

    Fixed brackets and semicolons

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,18 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace RazorPagesPizza.Models;
-
-public class Pizza
+namespace RazorPagesPizza.Models
 {
-    public int Id { get; set; }
-    
-    [Required]
-    public string? Name { get; set; }
-    public PizzaSize Size { get; set; }
-    public bool IsGlutenFree { get; set; }
+    public class Pizza
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string? Name { get; set; }
+        public PizzaSize Size { get; set; }
+        public bool IsGlutenFree { get; set; }
 
-    [Range(0.01, 9999.99)]
-    public decimal Price { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+    }
+
+    public enum PizzaSize { Small, Medium, Large }
 }
-
-public enum PizzaSize { Small, Medium, Large }
